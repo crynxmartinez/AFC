db7ca29c9c2723498271c0e60c27cbb664a5221c
crynxmartinez <crynxmartinez@example.com>
2025-11-24 07:50:23 +0800

    Add secure_url and image type meta tags for better Facebook compatibility

--- src/pages/EntryDetailPage.tsx
+++ src/pages/EntryDetailPage.tsx
@@ -133,8 +133,11 @@ export default function EntryDetailPage() {
         <meta property="og:title" content={entryTitle} />
         <meta property="og:description" content={entryDescription} />
         <meta property="og:image" content={entryImage} />
+        <meta property="og:image:secure_url" content={entryImage} />
+        <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content={entryTitle} />
         <meta property="og:site_name" content="Arena for Creatives" />
         
         {/* Twitter */}
