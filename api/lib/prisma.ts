d6ea475985b3330fe743d550343cc204788c775e
crynxmartinez <crynxmartinez@github.com>
2026-02-24 12:24:06 +0800

    Fix Prisma client - remove PrismaPg adapter incompatible with Accelerate URL

--- api/lib/prisma.ts
+++ api/lib/prisma.ts
@@ -1,19 +1,8 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import pg from 'pg'
 
 const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL || process.env.DIRECT_DATABASE_URL
-  
-  if (!connectionString) {
-    throw new Error('DATABASE_URL or DIRECT_DATABASE_URL must be set')
-  }
-
-  const pool = new pg.Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
-  
-  return new PrismaClient({ adapter }).$extends(withAccelerate())
+  return new PrismaClient().$extends(withAccelerate())
 }
 
 declare global {
